1f8019879fd8fa13ad2bbbe6011d48b6f242d102
Andrew Parsadayan <zed_oud@mac.com>
2018-10-20 16:11:03 -0700

    Update demoapp for opphack 2018 message
    Signed-off-by: Andrew Parsadayan <zed_oud@mac.com>

--- demoapp.py
+++ demoapp.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Attemtpting push
-
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -12,21 +9,23 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Hello, PayPal Opportunity Hack 2018!'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    html.Div(children=['''
+        This is built using ''',
+        html.A('Dash\n', href="https://plot.ly/products/dash/")]),
 
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Arizona'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': 'Chandler'},
+                {'x': [1, 2, 3], 'y': [3, 5, 1], 'type': 'line', 'name': 'Phoenix'},
+
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': 'Dash Data Visualization (dummy data)'
             }
         }
     )
